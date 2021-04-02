2eb21fe24fcb36e2ced326a1edcb420ebbb526b8
Alexandr Afonichev <afonyaafona@gmail.com>
2021-04-03 02:08:03 +0300

    push project

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="title">
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: ''
+      }
+    },
+
+    methods: {
+      onSubmit () {
+        if (this.title.trim()) {
+          const newTodo = {
+            id: Date.now(),
+            title: this.title,
+            completed:false
+          }
+          this.$emit('add-todo', newTodo)
+          this.title = ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: .5rem;
+  }
+
+  input {
+    flex: 0 0 90%;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  button {
+    font-size: 20px;
+    color: #fff;
+    text-transform: uppercase;
+    flex: 0 0 10%;
+    background: #22b325;
+    border: none;
+  }
+
+  button:active, button:focus {
+    outline: none;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <span :class="{ done: todo.completed }">
+      <input type="checkbox" @change="todo.completed = !todo.completed">
+      <strong>{{ index + 1 }}</strong>
+      {{ todo.title | uppercase }}
+    </span>
+    <button class="remove" @click="$emit('remove-todo', todo.id)">&times;</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      todo: {
+        type: Object,
+        required: true
+      },
+
+      index: Number
+    },
+
+    filters: {
+      uppercase (value) {
+        return value.toUpperCase()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  li {
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: .5rem 2rem;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    margin-right: 1rem;
+  }
+
+  .remove {
+    background: #f00;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: 800;
+  }
+
+  .remove:active, .remove:focus {
+    outline: none;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
